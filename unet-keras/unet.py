b4656ba7b88fe1558bda97e65bedb38fd8bcf077
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-02-24 22:57:20 +0200

    s

--- unet-keras/unet.py
+++ unet-keras/unet.py
@@ -153,8 +153,8 @@ class myUnet(object):
 
 		model = Model(input = inputs, output = conv10)
 
-		# model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
-		model.compile(optimizer = Adam(lr = 1e-4), loss = self.dice_coef_loss, metrics = [self.dice_coef])
+		model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+		# model.compile(optimizer = Adam(lr = 1e-4), loss = self.dice_coef_loss, metrics = [self.dice_coef])
 
 		return model
 
