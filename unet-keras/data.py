6544d24cb8a3585aaaabf208b250c55455dc588f
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-02-27 21:12:36 +0200

    S

--- unet-keras/data.py
+++ unet-keras/data.py
@@ -190,7 +190,7 @@ class dataProcess(object):
 		for name_id in range(len(imgs)):
 			names[name_id] = imgs[name_id][39:46]
 		print(len(imgs))
-		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
+		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,3), dtype=np.uint8)
 		for imgname in imgs:
 			midname = imgname[imgname.rindex("/")+1:]
 			img = load_img(self.test_path + "/" + midname)
