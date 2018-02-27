044e4f93c50e827a0f7bb95e15dbae99d948614a
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-02-27 20:34:24 +0200

    s

--- unet-keras/data.py
+++ unet-keras/data.py
@@ -156,8 +156,8 @@ class dataProcess(object):
 		print('-'*30)
 		imgs = glob.glob(self.data_path+"/*."+self.img_type)
 		print(len(imgs))
-		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
-		imglabels = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
+		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,3), dtype=np.uint8)
+		imglabels = np.ndarray((len(imgs),self.out_rows,self.out_cols,3), dtype=np.uint8)
 		for imgname in imgs:
 			midname = imgname[imgname.rindex("/")+1:]
 			# img = load_img(self.data_path + "/" + midname,grayscale = True)
