d95e02dec4c679a8778e77e20acd87c6dde45461
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-03-04 15:12:17 +0200

    s

--- a1/k1.py
+++ a1/k1.py
@@ -36,30 +36,30 @@ class myUnet(object):
         # pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
         # print("pool1 shape:", pool1.shape)
 
-        conv2 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+        conv2 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
         print("conv2 shape:", conv2.shape)
         conv2 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
         print("conv2 shape:", conv2.shape)
         # pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
         # print("pool2 shape:", pool2.shape)
 
-        conv3 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+        conv3 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
         print("conv3 shape:", conv3.shape)
         conv3 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
         print("conv3 shape:", conv3.shape)
         # pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
         # print("pool3 shape:", pool3.shape)
 
-        conv4 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+        conv4 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
         conv4 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
         # drop4 = Dropout(0.5)(conv4)
         # pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-        conv5 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+        conv5 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
         conv5 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
         # drop5 = Dropout(0.5)(conv5)
 
-        conv10 = Conv2D(1, 1, activation='sigmoid')(drop5)
+        conv10 = Conv2D(1, 1, activation='sigmoid')(conv5)
         '''
         up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(drop5))
