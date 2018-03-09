4e1952cf74834b8659fb44400f74ea59eaef05d3
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-03-09 14:30:51 +0200

    Test

--- unet-keras/data.py
+++ unet-keras/data.py
@@ -14,9 +14,9 @@ class myAugmentation(object):
 	Finally, seperate augmentated image apart into train image and label
 	"""
 
-	def __init__(self, out_rows, out_cols, data_path="/content/melanoma.1.0/hassan_data/train/image",
-				 label_path="/content/melanoma.1.0/hassan_data/train/label",
-				 test_path="/content/melanoma.1.0/hassan_data/test", npy_path="/content/unet-keras/npydata",
+	def __init__(self, out_rows, out_cols, data_path="/content/melanoma.1.0/dataset/train/image",
+				 label_path="/content/melanoma.1.0/dataset/train/label",
+				 test_path="/content/melanoma.1.0/dataset/test", npy_path="/content/unet-keras/npydata",
 				 img_type="jpg"):
 		"""
 		Using glob to get all .img_type form path
