0db12b94fb7249c0818ad3a6405842b09a1338b8
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-03-09 15:56:32 +0200

    Test

--- unet-keras/losses.py
+++ unet-keras/losses.py
@@ -10,7 +10,7 @@ from .utils.generic_utils import deserialize_keras_object
 from .utils.generic_utils import serialize_keras_object
 
 
-def dice(self, y_true, y_pred, smooth=1):
+def dice(y_true, y_pred, smooth=1):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
