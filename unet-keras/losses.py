81ecb9e9fc51d3d39fcdaf14806fcf700b4b32bc
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-03-09 16:04:05 +0200

    Test

--- unet-keras/losses.py
+++ unet-keras/losses.py
@@ -16,6 +16,9 @@ def dice(y_true, y_pred, smooth=1):
     intersection = K.sum(y_true_f * y_pred_f)
     return -1 * (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+def dice_acc(y_true, y_pred, smooth=1):
+    return - dice(y_true, y_pred)
+
 def mean_squared_error(y_true, y_pred):
     return K.mean(K.square(y_pred - y_true), axis=-1)
 
