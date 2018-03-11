2924c452d18d9580f9e0ec9ca3cf29ff63284390
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-03-11 13:45:05 +0200

    Update Accuracy to jaccard

--- unet-keras/objectives.py
+++ unet-keras/objectives.py
@@ -14,7 +14,7 @@ def dice(self, y_true, y_pred, smooth=1):
 def dice_acc(y_true, y_pred, smooth=1):
     return - dice(y_true, y_pred)
 
-def calc_jaccard(y_true, y_pred):
-    num = np.sum(y_true[y_pred == 1])
-    den = np.sum(y_true == 1) + np.sum(y_pred == 1) - num
-    return num / den
+# def calc_jaccard(y_true, y_pred):
+  #  num = np.sum(y_true[y_pred == 1])
+   # den = np.sum(y_true == 1) + np.sum(y_pred == 1) - num
+    #return num / den
