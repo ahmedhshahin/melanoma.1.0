9c246fe6ba2c0f2c5dd11dfc7d5d6b4864540f61
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-03-16 15:33:45 +0200

    Change loss to soft dice

--- unet-keras/unet.py
+++ unet-keras/unet.py
@@ -178,11 +178,11 @@ class myUnet(object):
 		def soft_dice(y_pred, y_true):
     			# y_pred is softmax output of shape (num_samples, num_classes)
     			# y_true is one hot encoding of target (shape= (num_samples, num_classes))
-			y_true = K.empty((y_true.shape[0]*y_true.shape[1], 2))
+			y_true = K.zeros(shape=(y_true.shape[0]*y_true.shape[1], 2))
 			y_true[:,0] = K.flatten(y_true[..., 0])
 			y_true[:,1] = K.flatten(y_true[..., 1])
 
-			y_pred = K.empty((y_pred.shape[0]*y_pred.shape[1], 2))
+			y_pred = K.zeros(shape=(y_pred.shape[0]*y_pred.shape[1], 2))
 			y_pred[:,0] = K.flatten(y_pred[..., 0])
 			y_pred[:,1] = K.flatten(y_pred[..., 1])
 			
