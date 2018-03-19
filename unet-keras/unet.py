c3c32f1e84f9be14ee468a6e0a848f92d1be5f88
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-03-19 16:14:33 +0200

    s

--- unet-keras/unet.py
+++ unet-keras/unet.py
@@ -212,7 +212,7 @@ class myUnet(object):
 
 		model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss',verbose=1, save_best_only=True)
 		print('Fitting model...')
-		model.fit(imgs_train, imgs_mask_train, batch_size=16, nb_epoch=500, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint, history])
+		model.fit(imgs_train, imgs_mask_train, batch_size=16, nb_epoch=500, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
 		print('predict test data')
 		imgs_mask_test = model.predict(imgs_test, batch_size=1, verbose=1)
 		np.save('/content/unet-keras/results/imgs_mask_test.npy', imgs_mask_test)
