e6b0ff0894c30fcf7a6ce12d49b1b10b5dcad300
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-05-07 18:18:53 +0200

    full Resolution model experiment

--- model/main.py
+++ model/main.py
@@ -13,4 +13,4 @@ transformations = transforms.Compose([transforms.ToTensor(), transforms.Normaliz
 
 train_object = Training(model = FullResulotionNet, model_params = {'in_channels': 3, 'n_outputs': 1, 'n_filters' : 128}, criterion = soft_dice_loss, val_metric = val_metric, initial_lr = 5e-5, dataset = Melanoma, dataset_params = {'data_path': '/content/melanoma.1.0/dataset/2016data/train/', 'test_path' : '/content/melanoma.1.0/dataset/2016data/test/', 'transforms' : transformations, 'is_train' : True, 'is_test' : False}, batch_size_train = 4, train_steps_before_update = 4, batch_size_val = 4, cuda_device = 0, data_parallel = False, test_mode = False)
 
-train_object.train_model(50)
+train_object.train_model(5)
