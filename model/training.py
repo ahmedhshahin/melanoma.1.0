4a6dd7f4796805216efaf5666520d87306e6b081
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-05-09 20:03:54 +0200

    upload full size data

--- model/training.py
+++ model/training.py
@@ -163,7 +163,6 @@ class Training():
         cnt = 0
         for images, labels, size in self.val_loader:
             images = Variable(images, requires_grad=False).cuda(self.cuda_device)
-            print(images.size(0))
             pred[cnt:cnt+images.size(0)] = self.net(images).cpu().data.numpy()#.reshape(4, -1)
             # y[cnt:cnt+4] = labels.cpu().numpy().astype(np.uint8)#.reshape(4, -1)
             y.append(labels.cpu().numpy().astype(np.uint8))
@@ -173,7 +172,7 @@ class Training():
         # label_orgn_size = []
         score = 0.0
         for p in range(pred.shape[0]):
-            print(p)
+            print(pred.shape[0])
             img = rev_padding(pred[p][0], orgn_size[p])
             temp = np.zeros(img.shape)
             temp[img >= 0.5] = 1
