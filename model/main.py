7327cff68852798b092526270136b88b1bee89f9
ahmedshahin9 <ahmedhassan9922@gmail.com>
2018-05-12 17:14:46 +0200

    full Resolution model experiment

--- model/main.py
+++ model/main.py
@@ -13,7 +13,7 @@ import torch
 #torch.backends.cudnn.benchmark = True
 transformations = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.51892472, 0.4431646,  0.40640972], [0.37666158, 0.33505249, 0.32253156])])
 
-train_object = Training(model = FullResulotionNet, model_params = {'in_channels': 3, 'n_outputs': 1, 'n_filters' : 128}, criterion = soft_dice_loss, val_metric = val_metric, initial_lr = 5e-1, dataset = Melanoma, dataset_params = {'data_path': '/content/melanoma.1.0/dataset/overfit/', 'test_path' : '/content/melanoma.1.0/dataset/2016data/test/', 'transforms' : transformations, 'is_train' : True, 'is_test' : False}, batch_size_train = 1, train_steps_before_update = 4, batch_size_val = 1, cuda_device = 0, data_parallel = False, test_mode = False, overfit_mode = True)
+train_object = Training(model = FullResulotionNet, model_params = {'in_channels': 3, 'n_outputs': 1, 'n_filters' : 128}, criterion = soft_dice_loss, val_metric = val_metric, initial_lr = 5, dataset = Melanoma, dataset_params = {'data_path': '/content/melanoma.1.0/dataset/overfit/', 'test_path' : '/content/melanoma.1.0/dataset/2016data/test/', 'transforms' : transformations, 'is_train' : True, 'is_test' : False}, batch_size_train = 1, train_steps_before_update = 4, batch_size_val = 1, cuda_device = 0, data_parallel = False, test_mode = False, overfit_mode = True)
 
 train_object.train_model(200)
 train_object.predict_test(save_dir = '/content/melanoma.1.0/dataset/2016data/test/', thresh=0.5)
